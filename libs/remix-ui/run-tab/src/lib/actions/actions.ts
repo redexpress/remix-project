6b73acbeb5a242c8b33edf34a06973ac0fdb655c
Gavin Yang <redppl@hotmail.com>
2023-12-25 14:05:56 +0800

    Update actions.ts

--- libs/remix-ui/run-tab/src/lib/actions/actions.ts
+++ libs/remix-ui/run-tab/src/lib/actions/actions.ts
@@ -65,7 +65,7 @@ export const updateGasPrice = (dispatch: React.Dispatch<any>, price: string) => 
   dispatch(setGasPrice(price))
 }
 
-export const addInstance = (dispatch: React.Dispatch<any>, instance: { contractData?: ContractData, address: string, name: string, abi?: any, decodedResponse?: Record<number, any> }) => {
+export const addInstance = (dispatch: React.Dispatch<any>, instance: { contractData?: ContractData, address: string, name: string, abi?: any, abi2?: string, decodedResponse?: Record<number, any> }) => {
   instance.decodedResponse = {}
   dispatch(addNewInstance(instance))
 }
